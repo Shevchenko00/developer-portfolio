3590984ccfd1467f25e1cb589c0438b01b1abca1
ShevchenkoO <shevchenko.sasha2005@gmail.com>
2025-09-18 19:04:16 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import type {JSX} from "react";
+
+const App = ():JSX.Element => {
+    return (
+        <>
+            <h1>Hello world!</h1>
+        </>
+    );
+}
+export default App
